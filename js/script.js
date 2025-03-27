f6f56ad0d991e0fc84f06edf337e7e1f22550316
Erick Messias <erick.messias182@outlook.com>
2025-03-27 09:59:49 -0300

    adicionando animacao ao scroll

--- js/script.js
+++ js/script.js
@@ -71,3 +71,27 @@ function initScrollSuave() {
 }
 
 initScrollSuave();
+
+// Animacao ao scroll
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisisble = sectionTop - windowMetade < 0;
+        if (isSectionVisisble) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimacaoScroll();
