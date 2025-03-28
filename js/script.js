1a22da204a5c2ff0b42e71a640bcf4c825e7f481
Erick Messias <erick.messias182@outlook.com>
2025-03-28 10:07:23 -0300

    adicionando modal ao site

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab-nav.js";
+import initMoldal from "./modules/modal.js"
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
+initMoldal();
