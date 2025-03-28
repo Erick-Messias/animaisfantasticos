2b7cbe2b235abe9d4e69c0a46561a713eb3d88aa
Erick Messias <erick.messias182@outlook.com>
2025-03-28 00:56:11 -0300

    adicionando modules

---
+++ js/modules/scroll-animacao.js
@@ -0,0 +1,21 @@
+export default function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisisble = sectionTop - windowMetade < 0;
+        if (isSectionVisisble) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+    animaScroll();
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimacaoScroll();
